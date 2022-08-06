32c2b5d764f253a3e57a4a512e911f4e7e48f159
ArtemisX64 <jobinsjjc9312@protonmail.com>
2022-08-06 18:35:25 +0530

    Update Exports

--- exports/gl_3v3.zig
+++ exports/gl_3v3.zig
@@ -5012,7 +5012,7 @@ const function_pointers = struct {
     var glUniformBlockBinding: ?function_signatures.glUniformBlockBinding = null;
 };
 
-test "121" {
+test {
     _ = load;
     @setEvalBranchQuota(100_000); // Yes, this is necessary. OpenGL gets quite large!
     std.testing.refAllDecls(@This());
